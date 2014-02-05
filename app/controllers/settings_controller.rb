a5fd6eaf4b5801037120f200d6c1f0fc0095f9fc
derek zeng <derek.zeng@ymail.com>
2014-02-05 08:13:35 -0500

    save

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,16 +1,5 @@
 class SettingsController < ApplicationController
   before_filter :authenticate
   def index
-    categories
-    render "categories"
-  end
-
-  def categories
-    @categories = current_user.categories
-    @category = Category.new
-  end
-
-  def preference
-    
   end
 end
